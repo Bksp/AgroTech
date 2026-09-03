b2778c779a87d67a4eac1fb01906abfb7e31a236
Bksp <fabian.cares.d@gmail.com>
2026-09-03 15:50:51 -0400

    mvp 0.1.2b

--- AgroTech.UI/App.xaml.cs
+++ AgroTech.UI/App.xaml.cs
@@ -2,6 +2,8 @@ using System.Configuration;
 using System.Data;
 using System.Windows;
 
+using System;
+
 namespace AgroTech.UI
 {
     /// <summary>
@@ -9,5 +11,25 @@ namespace AgroTech.UI
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            this.DispatcherUnhandledException += App_DispatcherUnhandledException;
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+        }
+
+        private void App_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show($"Error (UI): {e.Exception.Message}\n\nStack: {e.Exception.StackTrace}", "Crash Report", MessageBoxButton.OK, MessageBoxImage.Error);
+            e.Handled = true;
+        }
+
+        private void CurrentDomain_UnhandledException(object sender, System.UnhandledExceptionEventArgs e)
+        {
+            if (e.ExceptionObject is System.Exception ex)
+            {
+                MessageBox.Show($"Error (Core): {ex.Message}\n\nStack: {ex.StackTrace}", "Crash Report", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
     }
 }
